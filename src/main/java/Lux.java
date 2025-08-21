15d6ace7973618ae020a315cf4ae6e70c9016037
donkoo24 <donkoo@live.com.sg>
2025-08-21 16:34:02 +0800

    Refactor code

--- src/main/java/Lux.java
+++ src/main/java/Lux.java
@@ -2,16 +2,25 @@ import java.util.Scanner;
 
 public class Lux {
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
+        greet();
+        handleConvo();
+        endConvo();
+    }
+
+    private static void greet() {
         System.out.println("Hello! I'm Lux\nWhat can I do for you?\n");
+    }
 
+    private static void handleConvo() {
+        Scanner userInput = new Scanner(System.in);
         String userInputInfo = userInput.nextLine();
-
         while (!userInputInfo.equalsIgnoreCase("bye")) {
             System.out.printf("%s\n\n", userInputInfo);
             userInputInfo = userInput.nextLine();
         }
+    }
 
+    private static void endConvo() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
