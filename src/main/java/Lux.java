f52173456ee4ac96f221123939ecb48740493cc0
donkoo24 <donkoo@live.com.sg>
2025-08-21 16:19:44 +0800

    Add support for echo

--- src/main/java/Lux.java
+++ src/main/java/Lux.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Lux {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Lux\nWhat can I do for you?\n\nBye. Hope to see you again soon!");
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Hello! I'm Lux\nWhat can I do for you?\n");
+
+        String userInputInfo = userInput.nextLine();
+
+        while (!userInputInfo.equalsIgnoreCase("bye")) {
+            System.out.printf("%s\n\n", userInputInfo);
+            userInputInfo = userInput.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
